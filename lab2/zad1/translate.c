d591d4904ee879375d3a5746ca4368827bc7d7a9
Paulina <paulina.jedrychowska@gmail.com>
2023-03-28 21:38:41 +0200

    25%

--- lab2/zad1/translate.c
+++ lab2/zad1/translate.c
@@ -9,12 +9,36 @@
   #include <sys/stat.h>
 #endif
 
-long get_file_size(FILE* fp) {
-    long size;
-	fseek(fp, 0, SEEK_END);
-	size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	return size;
+#include <sys/times.h>
+#include <unistd.h>
+clock_t start_time, end_time;
+struct tms start_buffer, end_buffer;
+double mean_time;
+double mean_time_utime;
+double mean_time_stime;
+int TEST_NUMBER = 100;
+
+void start_timer(){
+	start_time = times(&start_buffer);
+}
+
+void stop_timer(){
+	end_time = times(&end_buffer);
+}
+
+double calc_time(clock_t start, clock_t end) {
+    return ((double) (end - start) / sysconf(_SC_CLK_TCK));
+}
+
+void add_mean_times(){
+  mean_time += calc_time(start_time, end_time);
+  mean_time_utime += calc_time(start_buffer.tms_utime, end_buffer.tms_utime);
+  mean_time_stime += calc_time(start_buffer.tms_stime, end_buffer.tms_stime);
+}
+
+void print_times(){
+	printf("TIMES: real %.3fs user %.3fs sys %.3fs\n",
+        mean_time/TEST_NUMBER, mean_time_utime/TEST_NUMBER, mean_time_stime/TEST_NUMBER);
 }
 
 int main (int argc, char* argv[])
@@ -38,42 +62,53 @@ int main (int argc, char* argv[])
   char *out_file_name = argv[4];
 
   #ifdef SYS
-    int in_file = open(in_file_name, O_RDONLY);
-    int out_file = open(out_file_name, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    for(int i = 0; i < TEST_NUMBER; i++){
+      start_timer();
+      int in_file = open(in_file_name, O_RDONLY);
+      int out_file = open(out_file_name, O_CREAT | O_TRUNC | O_WRONLY, 0644);
 
-    char* buff = calloc(1, sizeof(char));
-      while (read(in_file, buff, sizeof(char)) == 1){
-          if(*buff != old_ascii[0]){
-            write(out_file, buff, sizeof(char));
-          }
-          else{
-            write(out_file, new_ascii, sizeof(char));
-          }
-      }
-      
+      char* buff = calloc(1, sizeof(char));
+        while (read(in_file, buff, sizeof(char)) == 1){
+            if(*buff != old_ascii[0]){
+              write(out_file, buff, sizeof(char));
+            }
+            else{
+              write(out_file, new_ascii, sizeof(char));
+            }
+        }
+        
       free(buff);
-    close(in_file);
-    close(out_file);
+      close(in_file);
+      close(out_file);
+      stop_timer();
+      add_mean_times();
+    }
+    print_times();
   #endif
 
   #ifdef LIB
-    FILE* in_file = fopen (in_file_name, "r");
-    FILE* out_file = fopen(out_file_name, "w+");
+    for(int i = 0; i < TEST_NUMBER; i++){
+      start_timer();
+      FILE* in_file = fopen (in_file_name, "r");
+      FILE* out_file = fopen(out_file_name, "w+");
 
-    char* buff = calloc(1, sizeof(char));
-      while (fread(buff, sizeof(char), 1, in_file) == 1){
-          if(*buff != old_ascii[0]){
-            fwrite(buff, sizeof(char), 1, out_file);
-          }
-          else{
-            fwrite(new_ascii, sizeof(char), 1, out_file);
-          }
-      }
-      
+      char* buff = calloc(1, sizeof(char));
+        while (fread(buff, sizeof(char), 1, in_file) == 1){
+            if(*buff != old_ascii[0]){
+              fwrite(buff, sizeof(char), 1, out_file);
+            }
+            else{
+              fwrite(new_ascii, sizeof(char), 1, out_file);
+            }
+        }
+        
       free(buff);
       fclose(in_file);
       fclose(out_file);
-
+      stop_timer();
+      add_mean_times();
+    }
+    print_times();
   #endif
 
   return 0;
